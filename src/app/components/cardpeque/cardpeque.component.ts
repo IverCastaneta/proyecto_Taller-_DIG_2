7ea500ff9d94b2b620c403a10c8a4855260c4643
Iwer <iver.castaneta@gmail.com>
2024-11-11 17:21:49 -0400

    creacion de cardpeque

---
+++ src/app/components/cardpeque/cardpeque.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardpeque',
+  templateUrl: './cardpeque.component.html',
+  styleUrls: ['./cardpeque.component.scss'],
+})
+export class CardpequeComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
