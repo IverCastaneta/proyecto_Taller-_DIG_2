c1aae8abf26d71c0808a9e3edc9bc0dfe83e79a5
Iwer <iver.castaneta@gmail.com>
2024-11-13 20:48:16 -0400

    Subida de archivo a firebase

--- src/app/pages/ui-components/ui-components.page.ts
+++ src/app/pages/ui-components/ui-components.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-ui-components',
@@ -7,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UiComponentsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public db: DatabaseService
+  ) { 
+    /*this.loadData();*/
+  }
 
   ngOnInit() {
     console.log()
   }
 
+  loadData() {
+    this.db.fetchLocalCollection('lugares')
+      .subscribe((res: any) => {
+        console.log('salida lugares', res)
+        //this.db.addFirestoreDocument()
+        res.forEach((e: any) => {
+          console.log(e);
+          this.db.addFirestoreDocument('lugares', e)
+        });
+      })
+  }
 }
