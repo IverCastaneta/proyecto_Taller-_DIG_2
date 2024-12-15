35b3d1a4531059902b49d35f218c6de87a6685de
Iwer <iver.castaneta@gmail.com>
2024-12-14 22:47:17 -0400

    Revervas mejoradas

--- src/app/pages/view-lugar/view-lugar.page.ts
+++ src/app/pages/view-lugar/view-lugar.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
@@ -11,9 +12,11 @@ export class ViewLugarPage implements OnInit {
 
   id: any;
   lugar: any;
+  @Input() data: any;
   constructor(
     public db: DatabaseService,
-    public activatedRoute: ActivatedRoute
+    public activatedRoute: ActivatedRoute,
+    public auth: AuthService
   ) { 
     this.id = this.activatedRoute.snapshot.paramMap.get('idLugar');
     console.log('id lugar', this.id)
@@ -24,6 +27,7 @@ export class ViewLugarPage implements OnInit {
     .subscribe((res: any)=>{
       this.lugar = res;
     })
+    console.log('Data desde view-lugar', this.data)
   }
 
 }
